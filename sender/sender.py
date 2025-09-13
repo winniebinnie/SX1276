b8d9421b7839e0ab85d1e2f1b8fce298f291b775
winnieBinnie <ritthanupahp_s@cmu.ac.th>
2025-09-13 09:08:35 +0700

    Changed pin numbers

--- sender/sender.py
+++ sender/sender.py
@@ -2,16 +2,15 @@ from machine import Pin
 import time, urandom as random
 from lora import SX1276
 
-# Heltec WiFi LoRa 32 V2
 LoRa_MISO_Pin = 19
-LoRa_MOSI_Pin = 27
-LoRa_SCK_Pin  =  5
-LoRa_CS_Pin   = 18
-LoRa_RST_Pin  = 14
-LoRa_DIO0_Pin = 26
+LoRa_MOSI_Pin = 23
+LoRa_SCK_Pin  = 18
+LoRa_CS_Pin   = 5
+LoRa_RST_Pin  = 17
+LoRa_DIO0_Pin = 27
 LoRa_DIO1_Pin = 35
 LoRa_DIO2_Pin = 34
-SPI_CH        =  1
+SPI_CH        = 2  # VSPI
 
 random.seed(11)
 channels2Hopping = [914_000_000+200_000 * random.randint(0,10) for i in range(128)] # 914~916 MHz
